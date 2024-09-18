6f38b1daa148667e582ed1866653f45c631bb2e5
rasel mridha <raselmridha792@gmail.com>
2024-09-18 07:06:44 +0600

    append on js

---
+++ append.js
@@ -0,0 +1,4 @@
+const placesList = document.getElementById('places-list');
+const li = document.createElement('li');
+li.innerText = 'pahartoli';
+placesList.appendChild(li);
--- dom.js
+++ dom.js
@@ -1,1 +1,10 @@
 // console.log("hello from js");
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+    section.style.border = '2px solid red';
+    section.style.marginTop = '10px';
+    section.style.padding = '10px';
+    section.style.borderRadius = '15px';
+    section.style.backgroundColor = 'lightgray';
+    section.style.color = 'black';
+}
